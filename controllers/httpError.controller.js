3cdce58bdf01d95954a594f77c387d79da9885be
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-06-04 21:45:58 +0000

    💋Patron MVC

---
+++ controllers/httpError.controller.js
@@ -0,0 +1,25 @@
+// Importa los módulos 
+const express = require('express');
+
+// Crea una instancia del router de Express
+const router = express.Router();
+
+// Controlador para el error
+router.use((req, res) => {
+  res.status(error).render('error', {
+    title: 'Error 404 - Página no encontrada',
+    message: 'La página que estás buscando no existe.'
+  });
+});
+
+// Controlador para otros errores
+router.use((err, req, res, next) => {
+  // Puedes personalizar este controlador para diferentes tipos de errores
+  res.status(500).render('error', {
+    title: 'Error en el servidor',
+    message: 'Ha ocurrido un error en el servidor. Por favor, intenta nuevamente más tarde.'
+  });
+});
+
+// Exporta el módulo del controlador
+module.exports = router;
--- routes/shop.route.js
+++ routes/shop.route.js
@@ -1,33 +1,21 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el gestor de rutas
-import path from 'path';
+// Importando Action funcion del controlador products
+import { getProducts } from '../controllers/products.controller.js'
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// Importando productos
-import { products } from './admin.route.js';
-
 // GET /
-router.get('/', (req, res)=>{
-  //mostrando productos en memoria
-  console.log(products);
-  console.log("📢 Sirviendo la ruta '/'");
-  res.render('shop', {shop: 'active', docTitle:"Shop",
-  viewStyle: '/css/product.css',
-  isProductsListEmpty: products.length === 0,
-  products
-});
-});
+router.get('/', getProducts);
 
 // GET /about
 router.get('/about', (req, res) => {
   console.log("📢 Sirviendo la ruta '/about'");
   // Se contesta al server
   res.send(`
-  <h1 style="color: teal">📃 About </h1>
-  <p style="color: #555">App for Fullstack Web Dev Course I!</p>
+    <h1> ✔ About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
   `);
 });
 
